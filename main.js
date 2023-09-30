2d9b717b76acc4d38771cffb0ae5b24d85835bb5
lesavantdon <144865003+lesavantdon@users.noreply.github.com>
2023-09-29 22:28:21 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.length);
+secretMessage.pop();
+//console.log(secretMessage);
+
+secretMessage.push('to', 'Program');
+secretMessage.splice(7,1,'right');
+secretMessage.splice(0,1,'Programming');
+secretMessage.splice(6,5,'know');
+console.log(secretMessage.join());
